b2376cddca828d997c53da994fba5354ee29b8bd
Muhammad Naufal <mnaufal75@gmail.com>
2021-06-05 22:02:56 +0700

    Login with JWT token

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,29 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
 router.post('/signup', async (req, res) => {
-  console.log("signup");
+  const userExist = await User.findOne({ username: req.body.username });
+  if (userExist) {
+    return res.status(400).send('Username is already used');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
   const user = new User({
     username: req.body.username,
-    password: req.body.password,
+    password: hashPassword,
     userFullname: req.body.userFullname
   });
-  await user.save();
-  res.send(user);
+
+  try {
+    const savedUser = await user.save();
+    res.send({ user: savedUser._id });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 router.post('/login', async (req, res) => {
-  console.log("login");
   const user = await User
     .findOne({
       username: req.body.username,
     })
-  if (user && user.password === req.body.password) {
-    console.log("Log in");
+  if (!user) {
+    return res.status(400).send('Username or password is wrong');
   }
-  res.send(user);
+
+  const password = await bcrypt.compare(req.body.password, user.password);
+  if (!password) {
+    return res.status(400).send('Username or password is wrong');
+  }
+
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_TOKEN_SECRET);
+  res.header('auth-token', token).send(token);
 });
 
 module.exports = router;
