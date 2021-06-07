fa092d8656a26cf2f4684e7599377d21ab1dd362
Muhammad Naufal <mnaufal75@gmail.com>
2021-06-07 14:13:32 +0700

    Show username and fullname on left bottom corner

--- client/src/components/LeftBar.js
+++ client/src/components/LeftBar.js
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CreateTweetModal from '../modals/CreateTweetModal';
 import { logout } from '../actions/index';
+
+const mapStateToProps = (state) => {
+  return {
+    username: state.username,
+    userFullname: state.userFullname,
+  }
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -11,7 +19,7 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-const LeftBar = ({ cookies }) => {
+const LeftBar = ({ cookies, username, userFullname }) => {
   const [showModal, setShowModal] = useState(false);
 
   const history = useHistory();
@@ -23,26 +31,30 @@ const LeftBar = ({ cookies }) => {
 
   return (
     <div className="container w-1/4 h-full flex flex-col">
-      <span>Home</span>
-      <span>Explore</span>
-      <span>Notifications</span>
-      <span>Messages</span>
-      <span>Bookmark</span>
-      <span>Lists</span>
-      <span>Profile</span>
-      <span>More</span>
-      <button onClick={() => setShowModal(!showModal)}>Tweet</button>
+      <div className="fixed top-0 flex flex-col">
+        <span>Home</span>
+        <span>Explore</span>
+        <span>Notifications</span>
+        <span>Messages</span>
+        <span>Bookmark</span>
+        <span>Lists</span>
+        <span>Profile</span>
+        <span>More</span>
+        <button onClick={() => setShowModal(!showModal)}>Tweet</button>
+      </div>
 
-      <div className="flex flex-row absolute bottom-0">
+      <div className="fixed flex flex-row bottom-0">
         <div className="flex-0 m-2">
           <img
             className="rounded-full shadow-inner h-16 w-16"
             src="https://lh3.googleusercontent.com/ogw/ADGmqu-UDWio0GOwllYgAv_0g3Sx0VOUNox7rC3H1ZBPvA=s83-c-mo" />
         </div>
         <div className="flex-1 m-2">
-          <span className="font-bold">mnaufal75</span>
+          <span className="font-bold">
+            <Link to={`/${username}`}>{userFullname} </Link>
+          </span>
           <br />
-          <span className="">@mnaufal75</span>
+          <span className="">@{username}</span>
           <br />
           <button className="mt-4 p-2 rounded-full bg-blue-400 text-white text-lg" onClick={handleLogout}>LOGOUT</button>
         </div>
@@ -53,4 +65,4 @@ const LeftBar = ({ cookies }) => {
   )
 }
 
-export default connect(null, mapDispatchToProps)(LeftBar);
+export default connect(mapStateToProps, mapDispatchToProps)(LeftBar);
