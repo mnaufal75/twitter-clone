0064a79791637260224949a8917db76cdceddd71
Muhammad Naufal <mnaufal75@gmail>
2023-08-02 12:32:17 +0700

    Update import

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,43 @@
-import { useEffect } from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from "react";
+import { withCookies } from "react-cookie";
+import { connect } from "react-redux";
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
   Redirect,
+  Route,
+  Switch,
 } from "react-router-dom";
-import { withCookies } from 'react-cookie';
 
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import LeftBar from './components/LeftBar';
-import RightBar from './components/RightBar';
-import Status from './components/Status';
-import Profile from './components/Profile';
-import Home from './components/Home';
-import { setToken, getUserData } from '../src/actions/index';
+import { getUserData, setToken } from "../src/actions/index";
+import {
+  Home,
+  LeftBar,
+  Login,
+  Profile,
+  RightBar,
+  SignUp,
+  Status,
+} from "./components/";
 
 const mapStateToProps = (state) => {
   return {
     token: state.token,
-  }
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     setToken: (token) => dispatch(setToken(token)),
     getUserData: (token) => dispatch(getUserData(token)),
-  }
+  };
 };
 
 const App = ({ cookies, token, setToken, getUserData }) => {
   useEffect(() => {
     (async () => {
-      const tokenCookies = cookies.get('token') || token;
+      const tokenCookies = cookies.get("token") || token;
       setToken(tokenCookies);
-      cookies.set('token', token, { path: '/' });
+      cookies.set("token", token, { path: "/" });
     })();
   }, [token]);
 
@@ -47,7 +49,7 @@ const App = ({ cookies, token, setToken, getUserData }) => {
 
   return (
     <Router>
-      <div className="App container min-h-screen h-auto mx-auto px-16 flex">
+      <div className="App w-3/4 min-h-screen h-auto mx-auto px-16 flex">
         <Switch>
           <Route exact path="/">
             <Redirect to="/login" />
@@ -77,6 +79,6 @@ const App = ({ cookies, token, setToken, getUserData }) => {
       </div>
     </Router>
   );
-}
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(withCookies(App));
---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import LeftBar from "./LeftBar";
+import Login from "./Login";
+import Profile from "./Profile";
+import RightBar from "./RightBar";
+import SignUp from "./SignUp";
+import Status from "./Status";
+
+export { Login, SignUp, LeftBar, RightBar, Status, Profile, Home };
